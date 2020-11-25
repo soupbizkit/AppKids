b45c3c896f773e9aaf89176486e11efcd297b6b9
Walter <walter.santacruz@correounivalle.edu.co>
2020-11-25 12:25:23 -0500

    segunda pantalla ya se muestra para cada cultura

--- app_kids/lib/src/pages/mayas_page.dart
+++ app_kids/lib/src/pages/mayas_page.dart
@@ -1,26 +1,25 @@
 import 'package:flutter/material.dart';
 
-import 'package:app_kids/src/pages/incas_page.dart';
-import 'package:app_kids/src/pages/mayas_page.dart';
-import 'package:app_kids/src/pages/aztecas_page.dart';
+class MayasPage extends StatelessWidget {
 
-class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.green[100],
       appBar: AppBar(
               backgroundColor: Colors.yellow[700],
-              title: Text('América Precolombina', style: TextStyle(color: Colors.black)), 
+              title: Text('Mayas', style: TextStyle(color: Colors.black)), 
       ), //AppBar
       body: Center(
-      child: _crearBotones(context)
+      child: _botonesMayas()
       )
     );
   }
+}
 
 
-  Widget _crearBotones(context){
+
+Widget _botonesMayas(){
     return Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
@@ -31,15 +30,14 @@ class HomePage extends StatelessWidget {
               padding: EdgeInsets.all(20.0),
               splashColor: Colors.red[300],
               onPressed: (){
-                print("Cliquié Mayas");
-                 Navigator.of(context).push(MaterialPageRoute(
-                  builder: (context) => MayasPage(),
-                ));
+                print("Cliquié Ubicación");
               }, 
               child: Text(
-                'Mayas',
+                'Ubicación',
                 style: TextStyle(fontSize: 40.0))),
+
             SizedBox(height: 20),
+
             RaisedButton(
               elevation: 5.0,
               color: Colors.yellow[700],
@@ -47,15 +45,14 @@ class HomePage extends StatelessWidget {
               padding: EdgeInsets.all(20.0),
               splashColor: Colors.red[300],
               onPressed: (){
-                print("Cliquié Aztecas");
-                 Navigator.of(context).push(MaterialPageRoute(
-                  builder: (context) => AztecasPage(),
-                ));
+                print("Cliquié Social");
               }, 
               child: Text(
-                'Aztecas',
+                'Social',
                 style: TextStyle(fontSize: 40.0))),
+
             SizedBox(height: 20),
+
             RaisedButton(
               elevation: 5.0,
               color: Colors.yellow[700],
@@ -63,16 +60,26 @@ class HomePage extends StatelessWidget {
               padding: EdgeInsets.all(20.0),
               splashColor: Colors.red[300],
               onPressed: (){
-                print("Cliquié Incas");
-                Navigator.of(context).push(MaterialPageRoute(
-                  builder: (context) => IncasPage(),
-                ));
+                print("Cliquié Dios");
               }, 
               child: Text(
-                'Incas',
-                style: TextStyle(fontSize: 40.0)))
+                'Dioses',
+                style: TextStyle(fontSize: 40.0))),
+
+            SizedBox(height: 20),
+
+            RaisedButton(
+              elevation: 5.0,
+              color: Colors.yellow[700],
+              textColor: Colors.black,
+              padding: EdgeInsets.all(20.0),
+              splashColor: Colors.red[300],
+              onPressed: (){
+                print("Cliquié Reto");
+              }, 
+              child: Text(
+                'Reto',
+                style: TextStyle(fontSize: 40.0))),
           ],
         );
   }
-
-}
